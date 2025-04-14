3c1550b5e086d902b2556e85aac91a0b1c13f202
Daviluca05 <davilucaoliveira5@gmail.com>
2025-04-14 10:19:33 -0300

    corrigida a região para us-east-2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "instance_type" {
@@ -7,7 +7,8 @@ variable "instance_type" {
 }
 
 variable "ami" {
-  default = "ami-0d866da98d63e2b42"
+  default = "ami-0d866da98d63e2b42
+"
 }
 
 variable "key_name" {
