61cc62c915acbb17aa4c29beff62ba4e99ae3c02
Daviluca05 <davilucaoliveira5@gmail.com>
2025-04-14 00:32:25 -0300

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  default = "ami-0d866da98d63e2b42"
+}
+
+variable "key_name" {
+  default = "fastapi_key"
+}
+
+variable "docker_image" {
+  default = "ronierisonmaciel/login-app:latest"
+}
